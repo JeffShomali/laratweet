2b03f8e8e35aca755b205d22ca5183a1f3842adb
Jeff Shomali <jeffshomali@gmail.com>
2018-09-18 22:27:46 -0700

    Create Post and Response JSON

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostController extends Controller
 {
-    public function create(Request $request)
+    public function create(Request $request, Post $post)
     {
-        return 'post will be created';
+        // Create a new post by relationship
+        $createdPost = $request->user()->posts()->create([
+            'body' => $request->body,
+        ]);
+
+        // Return the reponse
+        return response()->json($post->with('user')->find($createdPost->id));
     }
 }
