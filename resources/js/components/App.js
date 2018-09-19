f31a6f8a6236e66b4ff83b140a2ae248c6eeca0f
Jeff Shomali <jeffshomali@gmail.com>
2018-09-18 21:02:59 -0700

    Create form in react component

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -5,7 +5,22 @@ class App extends Component {
         return (
             <div className="container">
                 <div className="row justify-content-center">
-                    <div className="col-md-8">
+                    <div className="col-md-6">
+                        <div className="card">
+                            <div className="card-header">Tweeet Something...</div>
+
+                            <div className="card-body">
+                                <form>
+                                    <div className="form-group">
+                                        <textarea className="form-control" rows="5" maxLength="140" placeholder="What's up?" required />
+                                    </div>
+                                    <input type="submit" value="Post" className="form-control" />
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                     <div className="col-md-6">
                         <div className="card">
                             <div className="card-header">App Component! </div>
 
