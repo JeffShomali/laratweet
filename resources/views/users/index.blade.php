a0b33b98e520d349a29579968fb3d1005fbbb8cc
Jeff Shomali <jeffshomali@gmail.com>
2018-09-20 19:46:53 -0700

    Add Following and Unfollowing Functionality in User Model and Index View

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    {{ $user->username }}
+
+    <hr>
+    @if(Auth::user()->isNotTheUser($user))
+        @if(Auth::user()->isFollowing($user))
+                <a href="#"> Unfollow </a>
+        @else
+                <a href="#"> Follow </a>
+        @endif
+    @endif
+</div>
+@endsection
