17301723c6281486b6dab564610f340ba56b68df
Jeff Shomali <jeffshomali@gmail.com>
2018-09-21 20:11:46 -0700

    Revert "Using setInterval for live commiunication"
    This reverts commit fbc3274a9118ce7086773eb01450782ae9dc9f28.

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,6 +1,4 @@
-import React, {
-    Component
-} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 
 class App extends Component {
@@ -18,15 +16,13 @@ class App extends Component {
         this.renderPosts = this.renderPosts.bind(this)
     }
 
-    getPosts() {
-        // this.setState({
-        //     loading: true
-        // })
+    getPosts(){
+        this.setState({loading: true})
         axios.get('/posts').then(response => {
             console.log(response.data.posts)
-            this.setState({
+            this.setState( {
                 posts: [...response.data.posts],
-                // loading: false
+                loading: false
             })
         })
     }
@@ -36,23 +32,13 @@ class App extends Component {
         this.getPosts();
     }
 
-    componentDidMount() {
-        this.interval = setInterval(() => {
-            this.getPosts()
-        }, 10000)
-    }
-
-    componentWillUnmount() {
-        clearInterval(this.interval)
-    }
-
-    handleSubmit(e) {
+    handleSubmit(e){
         e.preventDefault()
         // this.postData()
         axios.post('/posts', {
             body: this.state.body,
         }).then(response => {
-            // console.log(response)
+            console.log(response)
             this.setState({
                 posts: [...this.state.posts, response.data]
             })
@@ -64,108 +50,71 @@ class App extends Component {
 
     }
 
+    // postData(){
+    //     axios.post('/posts',{
+    //         body: this.state.body
+    //     })
+    // }
 
-    handleChange(e) {
+    handleChange(e){
         this.setState({
             body: e.target.value
         })
     }
 
-    renderPosts() {
-        return this.state.posts.map(post =>
-            <
-            div key = {
-                post.id
-            }
-            className = "media" >
-            <
-            div className = "media-left" >
-            <
-            img src = {
-                post.user.avatar
-            }
-            className = "media-object mr-2 " / >
-            <
-            /div> <
-            div className = "media-body" >
-            <
-            div className = "user" >
-            <
-            a href = {
-                `/users/${post.user.username}`
-            } > < b > {
-                post.user.username
-            } < /b></a >
-            <
-            /div> <
-            p > {
-                post.body
-            } < /p> < /
-            div > <
-            /div>
+    renderPosts(){
+       return this.state.posts.map(post =>
+            <div key={post.id} className="media">
+                    <div className="media-left">
+                        <img src={post.user.avatar} className="media-object mr-2 "/>
+                    </div>
+                    <div className="media-body">
+                        <div className="user">
+                            <a href={`/users/${post.user.username}`}><b>{post.user.username}</b></a>
+                        </div>
+                        <p>{post.body}</p>
+                    </div>
+            </div>
         )
     }
 
 
 
     render() {
-        return ( <
-            div className = "container" >
-            <
-            div className = "row justify-content-center" >
-            <
-            div className = "col-md-6" >
-            <
-            div className = "card" >
-            <
-            div className = "card-header" > Tweeet Something... < /div>
+        return (
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-md-6">
+                        <div className="card">
+                            <div className="card-header">Tweeet Something...</div>
 
-            <
-            div className = "card-body" >
-            <
-            form onSubmit = {
-                this.handleSubmit
-            } >
-            <
-            div className = "form-group" >
-            <
-            textarea onChange = {
-                this.handleChange
-            }
-            className = "form-control"
-            rows = "5"
-            maxLength = "140"
-            placeholder = "What's up?"
-            value = {
-                this.state.body
-            }
-            required / >
-            <
-            /div> <
-            input type = "submit"
-            value = "Post"
-            className = "form-control" / >
-            <
-            /form> < /
-            div > <
-            /div> < /
-            div >
+                            <div className="card-body">
+                                <form onSubmit={this.handleSubmit}>
+                                    <div className="form-group">
+                                        <textarea onChange={this.handleChange}
+                                         className="form-control"
+                                         rows="5" maxLength="140"
+                                         placeholder="What's up?"
+                                         value ={this.state.body}
+                                         required />
+                                    </div>
+                                    <input type="submit" value="Post" className="form-control" />
+                                </form>
+                            </div>
+                        </div>
+                    </div>
 
-            <
-            div className = "col-md-6" >
-            <
-            div className = "card" >
-            <
-            div className = "card-header" > Recent Tweets < /div>
+                     <div className="col-md-6">
+                        <div className="card">
+                            <div className="card-header">Recent Tweets </div>
 
-            <
-            div className = "card-body" > {!this.state.loading ? this.renderPosts() : 'Loading...'
-            } <
-            /div> < /
-            div > <
-            /div> < /
-            div > <
-            /div>
+                            <div className="card-body">
+                                    {!this.state.loading ? this.renderPosts() : 'Loading...'}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         );
     }
 }
